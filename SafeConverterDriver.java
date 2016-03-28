41acb039eff75ea767ed00c61f7a6a90e41fb76f
okaysierra <scseacat@gmail.com>
2016-03-28 17:45:47 -0500

    Create SafeConverter and SafeConverterDriver.

---
+++ SafeConverterDriver.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class SafeConverterDriver
+{
+	private static void createAndDisplayGUI()
+	{
+		JFrame frame = new JFrame("Temperature Converter");
+		SafeConverter demo = new SafeConverter();
+
+		frame.getContentPane().add(demo.getContent());
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args)
+	{
+		createAndDisplayGUI();
+	}
+}
